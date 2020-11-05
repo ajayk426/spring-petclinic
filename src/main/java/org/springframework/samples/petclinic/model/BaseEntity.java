1d63d35aa04ca27d5c20e5a0b062dc531c7c058f
ajay426 <ajaykumark426@gmail.com>
2020-11-05 15:09:47 +0530

    first change

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -24,7 +24,7 @@ import javax.persistence.MappedSuperclass;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
- * needing this property.
+ * needing this property
  *
  * @author Ken Krebs
  * @author Juergen Hoeller
